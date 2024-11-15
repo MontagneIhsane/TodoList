0a5c7a736bfd5dc77f084fc4ff3cbd4580c8e047
MontagneIhsane <ihsane.montagne@gmail.com>
2024-11-15 11:59:42 +0100

    Edit js : add the elements

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ document
     .addEventListener("click", function() {
         console.log("click");
     });
+
+    let todoList =document.getElementById("todo-list");
+
+    todoList.innerHTML = "<li>Une Todo !</li>";
+    todoList.innerHTML += "<li>Une  autre Todo youpi.... !</li>";
+    todoList.innerHTML += "<li>Une  autre Todo side eyes ... !</li>";
