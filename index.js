e9f57facf73ae05bc8bf3623f119a83cd0435269
MontagneIhsane <ihsane.montagne@gmail.com>
2024-11-18 10:14:09 +0100

    Add IMG for the trash & Finish the add button

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 document
     .getElementById("button-add-todo")
     .addEventListener("click", function() {
-        console.log("click");
+        let todoInput = document.getElementById("todo-text-input");
+        todoList.innerHTML += `<li>${todoInput.value}</li>`;
+
+
+        //console.log("click");
     });
 
     let todoList =document.getElementById("todo-list");
 
-    todoList.innerHTML = "<li>Une Todo !</li>";
-    todoList.innerHTML += "<li>Une  autre Todo youpi.... !</li>";
-    todoList.innerHTML += "<li>Une  autre Todo side eyes ... !</li>";
+    
+
+    //todoList.innerHTML += "<li>Une Todo !</li>";
+    //todoList.innerHTML += "<li>Une  autre Todo youpi.... !</li>";
+    //todoList.innerHTML += "<li>Une  autre Todo side eyes ... !</li>";
+
+
+    const clearbutton = document.getElementById("clear-button");
+    clearbutton.addEventListener("click", function() {
+        todoList.innerHTML = "";
+    });
